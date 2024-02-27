f4e9b7bbc7d6a92ccb9b6fa2a9fea406b41c145d
徐靚倪 <rilv1005@gmail.com>
2024-02-27 21:03:58 +0800

    作業

--- 20240227/11022306.py
+++ 20240227/11022306.py
@@ -1,7 +1,7 @@
 print("Hello world")
 
 import feedparser
-
+import csv
 def fetch_rss_titles_and_contents(url):
     feed = feedparser.parse(url)
     entries = [(entry.title, entry.summary) for entry in feed.entries]
@@ -11,12 +11,14 @@ url = "https://news.pts.org.tw/xml/newsfeed.xml"
 entries = fetch_rss_titles_and_contents(url)
 for title, content in entries:
     print(f"Title: {title}\nContent: {content}\n")
-    #檢查檔案的標題是否包含 台灣 如果有的話儲存成 excel 可讀取的格式
-    #放在 使用者的桌面上
-    #檔案名稱為 11022306.csv
-    if "台灣" in title:
-        with open("C:/Users/11022306/Desktop/11022306.csv", "a", encoding="utf-8") as f:
-            f.write(f"{title},{content}\n")
+    #列出台中市的新聞
+    if "台中市" in content:
+        print(f"Title: {title}\nContent: {content}\n")
+        with open("11022306.csv", "a", newline="", encoding="utf-8") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([title, content])
+            
+
     
     
 
