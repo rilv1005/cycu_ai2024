d760c917d9008988450d18ff7ef16f83a7f92314
徐靚倪 <rilv1005@gmail.com>
2024-03-05 20:32:13 +0800

    3.5第三節課

--- 20240305/oil.py
+++ 20240305/oil.py
@@ -2,53 +2,61 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# 獲取網頁內容
-url = "https://vipmbr.cpc.com.tw/mbwebs/ShowHistoryPrice_oil.aspx"
-response = requests.get(url)
+# 定義一個函數來抓取網頁上的表格資料
+def get_data(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    tables = soup.find_all('table')
+    df = pd.read_html(str(tables[1]))[0]
+    return df
 
-# 解析網頁內容
-soup = BeautifulSoup(response.text, 'html.parser')
+# 兩個網址
+url1 = "https://vipmbr.cpc.com.tw/mbwebs/ShowHistoryPrice_oil.aspx"
+url2 = "https://vipmbr.cpc.com.tw/mbwebs/ShowHistoryPrice_oil2019.aspx"
 
-# 找到所有的表格元素
-tables = soup.find_all('table')
+# 抓取兩個網址的資料
+df1 = get_data(url1)
+df2 = get_data(url2)
 
-# 將 HTML 表格轉換為 dataframe
-df1 = pd.read_html(str(tables[0]))[0]  # 第一個表格
-df2 = pd.read_html(str(tables[1]))[0]  # 第二個表格
+# 將兩個 dataframe 合併
+df = pd.concat([df1, df2])
 
-# 輸出結果
-print(df2)
+# 重設索引
+df.reset_index(drop=True, inplace=True)
 
-#做成csv檔案
-#儲存到桌面
-df2.to_csv('C:/Users/USER/Desktop/oil.csv', encoding='utf_8_sig')
+# 儲存到 CSV 檔案
+df.to_csv('C:/Users/USER/Desktop/oil2.csv', encoding='utf_8_sig')
 
-#df2 只保留前5個欄位的資料
-df2 = df2.iloc[:, :5]
-#去除欄位值為NAN的資料
-df2 = df2.dropna()
-#把第一欄位的資料型態 轉成 datetime
-df2.iloc[:, 0] = pd.to_datetime(df2.iloc[:, 0])
-#使用 matplotlib 繪製圖表，並設定圖表標題、x 軸是日期、y 軸油價
-#除了第一欄位資料，其他每一個欄位資料繪製一條線
+
+# 只保留前6個欄位的資料
+df = df.iloc[:, :6]
+# 去除值是NaN的資料
+df = df.dropna()
+
+# 把第一欄的資料型態
+
+# 把第一欄的資料型態 轉成 yyyy-mm-dd
+df[df.columns[0]] = df[df.columns[0]].str.replace('年', '-').str.replace('月', '-').str.replace('日', '')
+df[df.columns[0]] = pd.to_datetime(df[df.columns[0]])
+
+
+#繪製折線圖
+#使用繁體中文字型
+#x軸是日期 y軸是油價 
+#分別是 92無鉛汽油,95無鉛汽油,98無鉛汽油,超級柴油
+#除了日期以外的欄位 其他每個欄位都是一條折線
 import matplotlib.pyplot as plt
-from matplotlib.font_manager import FontProperties
-
-# 設定字體為 SimHei
-myfont = FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=14)
+plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
 plt.rcParams['axes.unicode_minus'] = False
-
-# 繪製圖表
 plt.figure(figsize=(10, 6))
-plt.title('CPC 油價走勢圖', fontproperties=myfont)
-plt.xlabel('日期', fontproperties=myfont)
-plt.ylabel('油價', fontproperties=myfont)
-for i in range(1, len(df2.columns)):
-    plt.plot(df2.iloc[:, 0], df2.iloc[:, i], label=df2.columns[i])
-plt.legend(prop=myfont)
+plt.plot(df[df.columns[0]], df[df.columns[1]], label='92無鉛汽油')
+plt.plot(df[df.columns[0]], df[df.columns[2]], label='95無鉛汽油')
+plt.plot(df[df.columns[0]], df[df.columns[3]], label='98無鉛汽油')
+plt.plot(df[df.columns[0]], df[df.columns[4]], label='超級柴油')
+plt.legend()
 plt.show()
-#將圖表轉為圖片 並儲存到桌面
-plt.savefig('C:/Users/USER/Desktop/oil.png')
+
+
 
 
 
