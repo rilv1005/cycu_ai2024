12ef0014f2c7ac3676a9c4af4899e3f14dc4a1db
徐靚倪 <160596543+rilv1005@users.noreply.github.com>
2024-04-30 10:34:17 +0000

    430-1

--- 20240423/期中作業.py
+++ 20240423/期中作業.py
@@ -1,23 +1,31 @@
-#https://tisvcloud.freeway.gov.tw/history/TDCS/M05A/20240416/
-import os
 import requests
 from bs4 import BeautifulSoup
+import os
+from datetime import timedelta, date
 
-def download_csv_from_url(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+# 定義一個函數來生成日期範圍
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
 
-    for link in soup.find_all('a'):
-        file_url = link.get('href')
-        if file_url.endswith('/'):  # 如果是資料夾，則遞迴訪問
-            download_csv_from_url(url + file_url)
-        elif file_url.endswith('.csv'):  # 如果是 csv 檔案，則下載
-            file_url = url + file_url
-            file_response = requests.get(file_url)
-            file_name = os.path.basename(file_url)
-            with open(file_name, 'wb') as f:
-                f.write(file_response.content)
+start_date = date(2024, 4, 15)
+end_date = date(2024, 4, 22)
 
-url = 'https://tisvcloud.freeway.gov.tw/history/TDCS/M05A/20240416/'
-download_csv_from_url(url)
+for single_date in daterange(start_date, end_date):
+    for h in range(24):
+        if h < 10:
+            base_url = 'https://tisvcloud.freeway.gov.tw/history/TDCS/M05A/' + single_date.strftime("%Y%m%d") + '/0' + str(h) + '/'
+        else:
+            base_url = 'https://tisvcloud.freeway.gov.tw/history/TDCS/M05A/' + single_date.strftime("%Y%m%d") + '/' + str(h) + '/'
 
+        for i in range(0, 5501, 500):
+            file_name = 'TDCS_M05A_' + single_date.strftime("%Y%m%d") + '_' + str(h).zfill(2) + str(i).zfill(4) + '.csv'
+            file_url = base_url + file_name
+
+            response = requests.get(file_url)
+
+            if response.status_code == 200:
+                with open('/workspaces/cycu_ai2024/20240423/' + file_name, 'wb') as file:
+                    file.write(response.content)
+            else:
+                print('Failed to download file: ' + file_url)
