bdde2f5f6fcfab1a8993ff307548d1fa172a373f
徐靚倪 <160596543+rilv1005@users.noreply.github.com>
2024-05-07 11:29:22 +0000

    5.7.3

--- 20240507/資料合併.py
+++ 20240507/資料合併.py
@@ -7,11 +7,11 @@ import pandas as pd
 df1 = pd.read_csv('/workspaces/cycu_ai2024/20240430/TDCS_M03A_combined-1.csv')
 df2 = pd.read_csv('/workspaces/cycu_ai2024/20240430/TDCS_M05A_combined.csv')
 
-# 合併兩個 DataFrame
-merged_df = pd.concat([df1, df2])
+# 從 df2 中選取第五欄位的資料
+df2_selected = df2.iloc[:, 4]
 
-# 移除重複的行
-merged_df = merged_df.drop_duplicates()
+# 將 df2 的第五欄位加到 df1
+df1['New_Column'] = df2_selected
 
 # 儲存合併後的資料
-merged_df.to_csv('merged.csv', index=False)
+df1.to_csv('merged.csv', index=False)
